3a48b8a0f2d1dc1a415ed51108bc2f38cc53be92
akrafts-gpt <akrafts.gpt@gmail.com>
2025-12-09 21:29:02 +0100

    Add keyboard setup guidance and handle insets

--- app/src/main/java/io/github/echoboard/HelloWorldKeyboardService.kt
+++ app/src/main/java/io/github/echoboard/HelloWorldKeyboardService.kt
@@ -3,11 +3,29 @@ package io.github.echoboard
 import android.inputmethodservice.InputMethodService
 import android.view.View
 import android.widget.Button
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class HelloWorldKeyboardService : InputMethodService() {
     override fun onCreateInputView(): View {
         val keyboardView = layoutInflater.inflate(R.layout.view_hello_world_keyboard, null)
         val helloWorldButton: Button = keyboardView.findViewById(R.id.buttonHelloWorld)
+        val initialLeftPadding = keyboardView.paddingLeft
+        val initialTopPadding = keyboardView.paddingTop
+        val initialRightPadding = keyboardView.paddingRight
+        val initialBottomPadding = keyboardView.paddingBottom
+
+        ViewCompat.setOnApplyWindowInsetsListener(keyboardView) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(
+                initialLeftPadding + systemBars.left,
+                initialTopPadding,
+                initialRightPadding + systemBars.right,
+                initialBottomPadding + systemBars.bottom
+            )
+            insets
+        }
+        ViewCompat.requestApplyInsets(keyboardView)
 
         helloWorldButton.setOnClickListener {
             currentInputConnection?.commitText(getString(R.string.hello_world_text), 1)
